b432528a7b0f0aeeb7f9debfecf56d59e4bd5c6a
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-08-07 09:54:12 +0600

    download page init

--- src/pages/download/__init__.py
+++ src/pages/download/__init__.py
@@ -1,6 +1,13 @@
-from flask import send_from_directory
+from flask import send_from_directory, render_template
 from config import config
+import os
+from src.utils.pdf import PdfEditor
+from src.const.messages import messages
 
 
 def download_view(filename):
+    full_path = os.path.join(config['UPLOAD_FOLDER'], '.'.join([filename, 'pdf']))
+    if not os.path.exists(full_path):
+        return render_template('message.html', message=messages['file_not_found'])
+
     return send_from_directory(config['UPLOAD_FOLDER'], filename='.'.join([filename, 'pdf']), as_attachment=True)
