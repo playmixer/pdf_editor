a823ed075b2548c61e28b8d3d5bfe7af3a237fe4
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-08-07 09:51:44 +0600

    setting for dev server

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
 config = {
-    "DEBUG": True,
-    "HOST": "localhost",
-    "PORT": 5000,
+    # Только для дев сервера
+    "DEV_DEBUG": True,
+    "DEV_HOST": "localhost",
+    "DEV_PORT": 5000,
+    #
+
     "ALLOWED_EXTENSIONS": ['pdf'],
     "TEMPLATES": os.path.join("src", "templates"),
     "STATIC": os.path.join("src", "static"),
     "UPLOAD_FOLDER": os.path.abspath(os.path.join(os.path.dirname(__file__), "upload")),
     "MAX_SIZE_FILE": 100,
     "FILE_NAME_LENGTH": 20,
-    "FILE_STORAGE_TIME": 3600,
+    "FILE_STORAGE_TIME": 600,   #60 = 1 мин
 }
