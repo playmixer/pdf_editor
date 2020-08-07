6c5cff73c48bbae44ac319f6b99901813ff7581c
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-08-07 09:52:22 +0600

    static count workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,9 @@
-#more settings
-#https://docs.gunicorn.org/en/latest/settings.html#settings
+# more settings
+# https://docs.gunicorn.org/en/latest/settings.html#settings
 
 # import multiprocessing
 
 bind = "0.0.0.0:8000"
+workers = 2
 #workers = multiprocessing.cpu_count() * 2 + 1
-workers = 3
+
