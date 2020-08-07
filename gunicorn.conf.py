89887735084e83878c1b3c3928f3f8926ec0b5a1
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-08-07 18:46:05 +0600

    set loglevel

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,7 @@
 # import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = 2
-#workers = multiprocessing.cpu_count() * 2 + 1
-
+workers = 3
+# workers = multiprocessing.cpu_count() * 2 + 1
+timeout = 120
+loglevel = 'info'
