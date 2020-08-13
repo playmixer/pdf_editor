48ab0a363eb0b0765630b7f31a54bd91014a4ced
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-08-13 10:52:04 +0600

    configure subdirectory

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ config = {
     "DEV_PORT": 5000,
     #
 
+    "SUBDIRECTORY": "",  # должен начинаться с '/', "" = без поддиректории
     "STATIC_URL_PATH": "/static",
     "ALLOWED_EXTENSIONS": ['pdf'],
     "TEMPLATES": os.path.join("src", "templates"),
@@ -14,7 +15,7 @@ config = {
     "UPLOAD_FOLDER": os.path.abspath(os.path.join(os.path.dirname(__file__), "upload")),
     "MAX_SIZE_FILE": 50,
     "FILE_NAME_LENGTH": 20,
-    "FILE_STORAGE_TIME": 600,  # 60 = 1 мин,
+    "FILE_STORAGE_TIME": 600,  # секунд,
     "SHOW_REAL_PAGE_IMAGE": True,  # большая нагрузка
     "LONG_TIME_LOADING_REAL_IMAGES": 10,
 }
--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from flask import Flask, render_template
 from config import config
 from src.pages import merge_pdf, remove_pages, organize_pdf, download, index, upload_file
 
+SUBDIR = config['SUBDIRECTORY']
+
 app = Flask(__name__,
             template_folder=config['TEMPLATES'],
             static_folder=config['STATIC'],
-            static_url_path=config['STATIC_URL_PATH']
+            static_url_path=''.join([SUBDIR, config['STATIC_URL_PATH']])
             )
 
 app.config['UPLOAD_FOLDER'] = config['UPLOAD_FOLDER']
 app.config['MAX_CONTENT_LENGTH'] = config['MAX_SIZE_FILE'] * 1024 * 1024
 
-app.add_url_rule('/home', view_func=index.index_view)
-app.add_url_rule('/', view_func=index.index_view)
-app.add_url_rule('/remove_pages', view_func=remove_pages.remove_pages_view, methods=['GET', 'POST'])
-app.add_url_rule('/merge_pdf', view_func=merge_pdf.merge_pdf_view, methods=['GET', 'POST'])
-app.add_url_rule('/organize_pdf', view_func=organize_pdf.organize_pdf_view, methods=['GET', 'POST'])
-app.add_url_rule('/download/<filename>', view_func=download.download_view)
-app.add_url_rule('/uploads/<filename>', view_func=upload_file.upload_file_view)
+app.add_url_rule(f'{SUBDIR}/home', view_func=index.index_view)
+app.add_url_rule(f'{SUBDIR}/', view_func=index.index_view)
+app.add_url_rule(f'{SUBDIR}/remove_pages', view_func=remove_pages.remove_pages_view, methods=['GET', 'POST'])
+app.add_url_rule(f'{SUBDIR}/merge_pdf', view_func=merge_pdf.merge_pdf_view, methods=['GET', 'POST'])
+app.add_url_rule(f'{SUBDIR}/organize_pdf', view_func=organize_pdf.organize_pdf_view, methods=['GET', 'POST'])
+app.add_url_rule(f'{SUBDIR}/download/<filename>', view_func=download.download_view)
+app.add_url_rule(f'{SUBDIR}/uploads/<filename>', view_func=upload_file.upload_file_view)
 
 
 @app.errorhandler(404)
