b3224a34927519fede73c3e9a460d2218135582b
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-08-24 09:38:56 +0600

    set default length

--- src/utils/rnd/__init__.py
+++ src/utils/rnd/__init__.py
@@ -3,8 +3,8 @@ import string
 from config import config
 
 
-def get_random_string():
-    length = config['FILE_NAME_LENGTH']
+def get_random_string(name_length=None):
+    length = name_length or config['FILE_NAME_LENGTH']
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
